224aa18877d94780d5f7085994f7748205f58936
Pablo Mattos <72421322+pablomattos@users.noreply.github.com>
2025-09-06 22:22:03 -0300

    Delete StartDB-2025 directory

--- StartDB-2025/src/core/processaEntrada.js
+++
@@ -1,6 +0,0 @@
-export function processaEntrada(brinquedosPessoa1, brinquedosPessoa2, listaAnimais) {
-  const p1 = brinquedosPessoa1.split(",").map(s => s.trim().toUpperCase());
-  const p2 = brinquedosPessoa2.split(",").map(s => s.trim().toUpperCase());
-  const ordemAnimais = listaAnimais.split(",").map(s => s.trim());
-  return { p1, p2, ordemAnimais };
-}
--- StartDB-2025/src/utils/validacoes.js
+++
@@ -1,3 +0,0 @@
-export function temDuplicados(arr) {
-  return new Set(arr).size !== arr.length;
-}
